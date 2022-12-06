da43b0522295dbfc9d393879bd2897cc6671482b
Attila Krizso <ITN236568@qualifizierung.at>
2022-12-06 14:20:16 +0100

    moviesCard2

--- eTickets/Controllers/MoviesController.cs
+++ eTickets/Controllers/MoviesController.cs
@@ -1,6 +1,7 @@
 ﻿using eTickets.Data;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
+using System.Linq;
 using System.Threading.Tasks;
 
 namespace eTickets.Controllers
@@ -16,7 +17,7 @@ namespace eTickets.Controllers
 
         public async Task<IActionResult> Index()
         {
-            var allMovies = await _context.Movies.ToListAsync();
+            var allMovies = await _context.Movies.Include(n => n.Cinema).OrderBy(n => n.Name).ToListAsync();
             return View(allMovies);
         }
     }
