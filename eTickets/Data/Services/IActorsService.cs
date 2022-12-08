59a0c8d726ff5b1d3a4e8f24d1e3354d50b91a71
Attila Krizso <ITN236568@qualifizierung.at>
2022-12-08 14:42:37 +0100

    Actors interface and service

---
+++ eTickets/Data/Services/IActorsService.cs
@@ -0,0 +1,19 @@
+﻿using eTickets.Models;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace eTickets.Data.Services
+{
+    public interface IActorsService
+    {
+        Task<IEnumerable<Actor>> GetAll();
+
+        Actor GetById(int id);
+
+        void Add(Actor actor);
+
+        Actor Update(int id, Actor newActor);
+
+        void Delete(int id);
+    }
+}
--- eTickets/Startup.cs
+++ eTickets/Startup.cs
@@ -1,4 +1,5 @@
 using eTickets.Data;
+using eTickets.Data.Services;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.EntityFrameworkCore;
@@ -27,6 +28,8 @@ namespace eTickets
             //DbContext configuration
             services.AddDbContext<AppDbContext>(options => options.UseSqlServer(Configuration.GetConnectionString("DefaultConnectionString")));
 
+            //Services configuration
+            services.AddScoped<IActorsService, ActorsService>();
 
 
             services.AddControllersWithViews();
