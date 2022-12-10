d42d2b5d1d3deddf691f18d7e6702d0710cdbc98
Attila Krizso <ITN236568@qualifizierung.at>
2022-12-10 23:22:08 +0100

    42.ik lecke vegeig

--- eTickets/Data/Services/IActorsService.cs
+++ eTickets/Data/Services/IActorsService.cs
@@ -1,19 +1,12 @@
-﻿using eTickets.Models;
+﻿using eTickets.Data.Base;
+using eTickets.Models;
 using System.Collections.Generic;
 using System.Threading.Tasks;
 
 namespace eTickets.Data.Services
 {
-    public interface IActorsService
+    public interface IActorsService : IEntityBaseRepository<Actor>
     {
-        Task<IEnumerable<Actor>> GetAllAsync();
-
-        Task<Actor> GetByIdAsync(int id);
-
-        Task AddAsync(Actor actor);
-
-        Task<Actor> UpdateAsync(int id, Actor newActor);
-
-        Task DeleteAsync(int id);
+        
     }
 }
