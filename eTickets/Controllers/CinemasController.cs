88262b1544ccc7bf4e3a78c36897ad887e9ea849
Attila Krizso <ITN236568@qualifizierung.at>
2022-12-11 17:35:55 +0100

    49 vegeig

--- eTickets/Controllers/CinemasController.cs
+++ eTickets/Controllers/CinemasController.cs
@@ -35,5 +35,13 @@ namespace eTickets.Controllers
             await _service.AddAsync(cinema);
             return RedirectToAction(nameof(Index));
         }
+
+        //Get: cinemas/details/id
+        public async Task<IActionResult> Details(int id)
+        {
+            var cinemaDetails = await _service.GetByIdAsync(id);
+            if (cinemaDetails == null) return View("NotFound");
+            return View(cinemaDetails);
+        }
     }
 }
