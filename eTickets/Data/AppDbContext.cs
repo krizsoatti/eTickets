a9bf91b79fd464ce5a03f00261ec4cf75c25d0cf
Attila Krizso <ITN236568@qualifizierung.at>
2022-12-12 13:42:01 +0100

    62 vegig kesz

--- eTickets/Data/AppDbContext.cs
+++ eTickets/Data/AppDbContext.cs
@@ -31,5 +31,11 @@ namespace eTickets.Data
         public DbSet<Cinema> Cinemas { get; set; }
         public DbSet<Producer> Producers { get; set; }
 
+
+        //Orders related tables
+        public DbSet<Order> Orders { get; set; }
+
+        public DbSet<OrderItem> OrderItems { get; set; }
+
     }
 }
