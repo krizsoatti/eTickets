44b2785ebd395b0462d0f49877e2a97474e99bd6
Attila Krizso <ITN236568@qualifizierung.at>
2022-12-14 13:10:33 +0100

    88 vegig

--- eTickets/Controllers/AccountController.cs
+++ eTickets/Controllers/AccountController.cs
@@ -46,5 +46,7 @@ namespace eTickets.Controllers
             TempData["Error"] = "Wrong credentials. Please, try again!";
             return View(loginVM);
         }
+
+        public IActionResult Register() => View(new RegisterVM());
     }
 }
